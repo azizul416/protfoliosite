550f32014d8b94a437c6eca79c5761018f6fa7a6
Md Azizul haque <104496955+azizul416@users.noreply.github.com>
2022-07-25 17:12:36 +0600

    I have upload a file

---
+++ Main.js
@@ -0,0 +1,7 @@
+var text = document.getElementById("tx");
+var button = document.getElementById("btn");
+var para = document.getElementById("demo");
+
+button.addEventListener("click", (e) => {
+  e.target.innerHtml ="";
+});
